432094a02686c9ddf37410d8fc97f173c0484c82
Mrak9087 <59642198+Mrak9087@users.noreply.github.com>
2020-09-24 21:54:28 +0400

    fix: dream-team should handle whitespaces

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,10 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
-  if (members === null || members === undefined) return false;
+  if (members === null || members === undefined || !Array.isArray(members)) return false;
   return members.map(
     item => {
         if (typeof item === 'string'){
+             item = item.trim();
              return item.slice(0,1).toUpperCase();
         }
     }

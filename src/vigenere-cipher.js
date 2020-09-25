3c21ae3275e0b449dcd4ecc41e8c0630186f22f3
Mrak9087 <59642198+Mrak9087@users.noreply.github.com>
2020-09-25 18:19:32 +0400

    feat: vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,56 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor (direct = true){
+    this.direct = direct;
+  }
+  encrypt(message, key) {
+    if (arguments.length < 2) throw new Error('arguments is undefined');
+    let messTmp = message.toUpperCase();
+    let keyTmp = key.toUpperCase();
+    let resArr = [];
+    let lenKey = key.length;
+    let indexKey = 0;
+    for (const ch of messTmp) {
+      let chCode = ch.charCodeAt(0);
+      if (chCode > 64 && chCode < 91){
+        let keyCode = keyTmp[indexKey++].charCodeAt(0);
+        if (keyCode > 64 && keyCode < 91){
+          resArr.push( String.fromCharCode(65 + ( (chCode + keyCode) % 26) ) );
+        }
+        if (indexKey >= lenKey){
+          indexKey = 0;
+        }
+      } else {
+        resArr.push(ch);
+      } 
+
+    }
+    return (this.direct)? resArr.join(''): resArr.reverse().join('');
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(message, key) {
+    if (arguments.length < 2) throw new Error('arguments is undefined');
+    let messTmp = message.toUpperCase();
+    let keyTmp = key.toUpperCase();
+    let resArr = [];
+    let lenKey = key.length;
+    let indexKey = 0;
+    for (const ch of messTmp) {
+      let chCode = ch.charCodeAt(0);
+      if (chCode > 64 && chCode < 91){
+        let keyCode = keyTmp[indexKey++].charCodeAt(0);
+        if (keyCode > 64 && keyCode < 91){
+          resArr.push( String.fromCharCode(65 + ( (chCode - keyCode + 26) % 26) ) );
+        }
+        if (indexKey >= lenKey){
+          indexKey = 0;
+        }
+      } else {
+        resArr.push(ch);
+      } 
+
+    }
+    return (this.direct)? resArr.join(''): resArr.reverse().join('');
   }
 }
 
